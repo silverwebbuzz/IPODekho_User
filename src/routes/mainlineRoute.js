5bf2a370f36283e1cfc03c703e35ed6a392b8d8d
Bhavik Koradiya <36161537+silverwebbuzz@users.noreply.github.com>
2023-01-30 12:17:21 +0530

    Update mainlineRoute.js

--- src/routes/mainlineRoute.js
+++ src/routes/mainlineRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Multer = require("multer");
 const mainlineIpo = require("../helper/MainLineIpo");
 
-router.get("/GetMainLineIpo", mainlineIpo.GetMainLineIpo);
-router.get("/GetIdByMainLineIpo/:id", mainlineIpo.GetIdByMainLineIpo);
+router.post("/GetMainLineIpo", mainlineIpo.GetMainLineIpo);
+router.post("/GetIdByMainLineIpo/:id", mainlineIpo.GetIdByMainLineIpo);
 // router.post("/uploadImage", FIREBASE_API.uploadFile);
 
 module.exports = {
